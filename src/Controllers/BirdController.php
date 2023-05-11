147a71542655f3705d6831d7a36ff421211d6929
Mathieu Le Ray <leraymathieu@gmail.com>
2023-05-11 16:07:06 +0200

    configuration et premiers tests php unit

--- src/Controllers/BirdController.php
+++ src/Controllers/BirdController.php
@@ -3,12 +3,34 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+/////////PROVISOIRE//////////////////
+require_once('../../config/global.php');
+//Autoloader composer
+//si modification relancer la commande composer dump-autoload
+require '../../vendor/autoload.php';
+/////////FIN DU PROVISOIRE///////////
 
+
+use App\Class\Bird;
 
 class BirdController
 {
+
+   private $bird;
+
    public function __construct()
    {
-       
+      $this->bird = new Bird();
+   }
+
+
+   public function birdInformations()
+   {
+      //PHP 8 on peut ignorer le premier paramètre / argument de la méthode getGeneralBirdInformations en nommant le deuxième paramètre sans le $
+       return $this->bird->getGeneralBirdInformations(currencySymbol: '$ : ');
    }
 }
+
+$bird = new BirdController();
+
+print $bird->birdInformations();
